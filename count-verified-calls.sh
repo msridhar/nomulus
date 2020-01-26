9b3068f478436f0211b984620cb1962ba32c6040
Manu Sridharan <msridhar@gmail.com>
2020-01-26 15:19:54 -0800

    fix script

--- count-verified-calls.sh
+++ count-verified-calls.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# note: requires ripgrep to have already been installed: https://github.com/BurntSushi/ripgrep
-
 echo "running build, be patient..."
-./gradlew clean compileJava -PcfLocal -PcfShowChecks > /tmp/output.txt
-COUNT=$(rg -U --count "build\(\)\n(.*)\n   expected: DECLARED @CalledMethods\(.*\n success" /tmp/output.txt)
-echo "${COUNT} verified calls"
+./gradlew clean compileJava -PcfLocal > /tmp/output.txt
+COUNT=$(cat /tmp/output.txt | grep "build()" | cut -f 2 -d ' ' | paste -sd+ - | bc)
+echo "${COUNT} calls to build"
